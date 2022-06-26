e47b4cb96070d81fca25d5da7bb11d4015832583
IJchenko <zdiml72@gmail.com>
2022-06-26 14:05:37 +0300

    add login and reg model for post action

---
+++ CallBoardNix/Models/LogRegModel.cs
@@ -0,0 +1,5 @@
+﻿namespace CallBoardNix.Models
+{
+    public record class LoginModel(string Email, string Password);
+    public record class RegisterModel(string Name, string Surname, string status, string PhoneNumber, string Email, string Password);
+}
