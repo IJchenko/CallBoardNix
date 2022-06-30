7bfa26e872630c5c66d5ea9c04595f596d6af753
IJchenko <zdiml72@gmail.com>
2022-06-30 22:04:56 +0300

    added controllers: [get] index - returns all advert to list, [get] CreateAdvet - returns all categories to list for creating advert in future, [post] CreateAdvert - for creating advert

--- CallBoardNix/Controllers/HomeController.cs
+++ CallBoardNix/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using BusinessLayer.DTO;
+﻿using AutoMapper;
+using BusinessLayer.DTO;
+using BusinessLayer.Interfaces;
 using CallBoardNix.Models;
+using DataLayer.EF;
 using DataLayer.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CallBoardNix.Controllers
@@ -9,77 +13,58 @@ namespace CallBoardNix.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        AdvertViewModel advert1;
-        UserViewModel user1;
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUserService _userService;
+        private readonly ICompanyService _companyService;
+        private readonly IMapper _mapper;
+        public HomeController(ILogger<HomeController> logger, IMapper mapper,IUserService userService,ICompanyService companyService)
         {
             _logger = logger;
-            advert1=new AdvertViewModel()
-            {
-                NameAdvert = ".Net",
-                Description = "text Description",
-                Requirements = "text Requirements",
-                Salary = "2000",
-                company = new Company() { CompanyName = "Nix", Description = "some text desc", Reviews = null, Link = "https://www.nixsolutions.com/ru/", URLImage = null, Users = null, Id = Guid.NewGuid() },
-                category = new Category() { Id = Guid.NewGuid(), CategoryName="C#", URLImage=null },
-            };
-            user1 = new UserViewModel()
-            {
-                Name = "Dmytro",
-                Surname = "Zin",
-                PhoneNumber = "0666055552",
-                Email = "dimas@gmail.com",
-                Resumes = null,
-                Companies = null,
-                Reviews = null
-            };
+            _mapper = mapper;
+            _userService = userService;
+            _companyService = companyService;
         }
-
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<ActionResult<List<AdvertView>>> Index()//повертає список всіх оголошень
         {
+            var models = await _companyService.GetAdvert();
+            var adverts = new List<AdvertView>();
+            foreach(var advert in models)
+            {
+                adverts.Add(_mapper.Map<AdvertView>(advert));
+            }
+            return View(adverts);
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<CategoryView>>> CreateAdvert()//повертає список всіх категорій
+        {
+            var models = await _companyService.GetCategory();
+            var categories = new List<CategoryView>();
+            foreach(var category in models)
+            {
+                categories.Add(_mapper.Map<CategoryView>(category));
+            }
+            ViewBag.Categories = categories;
             return View();
         }
-
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> CreateAdvert(AdvertView advert)//створює оголошення(не створює атрибут категорії)
         {
-            return View();
+            var model = _mapper.Map<AdvertDTO>(advert);
+            await _companyService.AddAdvert(model);
+            return View(advert);
         }
         public IActionResult Register()
         {
             return View();
         }
-        [HttpPost]
-        public string Register(RegisterModel model)
-        {
-            return $"{model.Name}--{model.Surname}--{model.status}--{model.PhoneNumber}--{model.Email}--{model.Password}--";
-        }
-
         public IActionResult Advert()
         {
             return View();
         }
-        [HttpGet]
-        public IActionResult Advert(AdvertViewModel advert)
-        {
-            advert= advert1;
-            ViewBag.Advert = advert;
-            return View();
-        }
-        [HttpGet]
-        public IActionResult Profile(UserViewModel user)
-        {
-            user= user1;
-            return View(user);
-        }
         public IActionResult Login()
         {
             return View();
-        }
-        [HttpPost]
-        public string Login(LoginModel login)
-        {
-            return $"{login.Email}------{login.Password}";
-        }
+        }      
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
