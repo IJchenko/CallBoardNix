e2bf10fefdf11c8eccd62039de035fbd16cf95fd
IJchenko <zdiml72@gmail.com>
2022-07-03 00:15:55 +0300

    changed view models on PL

--- CallBoardNix/Models/CompanyView.cs
+++ CallBoardNix/Models/CompanyView.cs
@@ -4,12 +4,9 @@ namespace CallBoardNix.Models
 {
     public class CompanyView
     {
-        public Guid Id { get; set; }
+        public Guid IdCompanyView { get; set; }
         public string? CompanyName { get; set; }
         public string? Description { get; set; }
         public string? Link { get; set; }
-        public List<Review>? Reviews { get; set; }
-        public List<User>? Users { get; set; }
-        public string? URLImage { get; set; }
     }
 }
