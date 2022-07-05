5d53c2ef6691729564b79d1b120f7969ede04a16
IJchenko <zdiml72@gmail.com>
2022-07-05 19:30:10 +0300

    removed user from map automapper

--- BusinessLayer/Extension/AutoMapperProfile.cs
+++ BusinessLayer/Extension/AutoMapperProfile.cs
@@ -18,7 +18,6 @@ namespace BusinessLayer.Extension
             CreateMap<Company, CompanyDTO>().ReverseMap();
             CreateMap<Resume, ResumeDTO>().ReverseMap();
             CreateMap<Review, ReviewDTO>().ReverseMap();
-            CreateMap<User, UserDTO>().ReverseMap();
         }
     }
 }
