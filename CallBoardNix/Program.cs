ed27c7645e671d1ccd4fc4b1e8a995b9d5144d22
IJchenko <zdiml72@gmail.com>
2022-07-05 19:37:34 +0300

    add identity

--- CallBoardNix/Program.cs
+++ CallBoardNix/Program.cs
@@ -3,7 +3,9 @@ using BusinessLayer.Interfaces;
 using BusinessLayer.Services;
 using CallBoardNix.Extentions;
 using DataLayer.EF;
+using DataLayer.Models;
 using DataLayer.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationContext>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile), typeof(AutoMapperViewProfile));
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ICompanyService, CompanyService>();
@@ -34,6 +36,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
