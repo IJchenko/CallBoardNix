b03184238df8b9081f047f7392f40dc9f283a902
IJchenko <zdiml72@gmail.com>
2022-07-15 12:15:55 +0300

    added pagination to list of company model

--- CallBoardNix/Models/Company/CompanyResultModel.cs
+++ CallBoardNix/Models/Company/CompanyResultModel.cs
@@ -3,14 +3,20 @@
     public class CompanyResultModel
     {
         public UserViewModel User { get; set; }
+        public List<CompanyView> Companys { get; set; }
         public CompanyView Company { get; set; }
-        public CompanyResultModel(UserViewModel user, CompanyView company)
+        public PaginationModel Pagination { get; set; }
+        public CompanyResultModel(UserViewModel user, CompanyView company, List<CompanyView> companys, PaginationModel pagination)
         {
+            Pagination = pagination;
+            Companys = companys;
             Company = company;
             User = user;
         }
-        public CompanyResultModel(UserViewModel user)
+        public CompanyResultModel(UserViewModel user, List<CompanyView> companys, PaginationModel pagination)
         {
+            Pagination = pagination;
+            Companys = companys;
             User = user;
         }
     }
