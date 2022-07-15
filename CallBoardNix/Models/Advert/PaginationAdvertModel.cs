f1d17f6dcb4164a3bd3de31c63cd8615b5a80e9a
IJchenko <zdiml72@gmail.com>
2022-07-15 12:14:57 +0300

    fix pagination model

--- CallBoardNix/Models/Advert/PaginationAdvertModel.cs
+++ CallBoardNix/Models/Advert/PaginationAdvertModel.cs
@@ -1,15 +1,20 @@
 ﻿namespace CallBoardNix.Models
 {
-    public class PaginationAdvertModel
+    public class PaginationModel
     {
         public int PageNumber { get; }
         public int TotalPages { get; }
         public bool HasPreviousPage => PageNumber > 1;
         public bool HasNextPage => PageNumber < TotalPages;
-        public PaginationAdvertModel(int count, int pageNumber, int pageSize)
+        public PaginationModel(int count, int pageNumber, int pageSize)
         {
             PageNumber = pageNumber;
             TotalPages = (int)Math.Ceiling(Convert.ToDecimal(count / pageSize));
+            int total = TotalPages;
+            if((total*pageSize)<count)
+            { 
+                TotalPages= total+1;
+            }
         }
     }
 }
