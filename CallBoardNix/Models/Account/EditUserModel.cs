325b2dafbad5c0d11ac82b12e99d4e6bd21d3c9c
IJchenko <zdiml72@gmail.com>
2022-07-18 20:40:47 +0300

    added editUser model

---
+++ CallBoardNix/Models/Account/EditUserModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CallBoardNix.Models.Account
+{
+    public class EditUserModel
+    {
+        [Required]
+        [StringLength(35, ErrorMessage = "You must fill your name", MinimumLength = 2)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(35, ErrorMessage = "You must fill your surname", MinimumLength = 2)]
+        public string? Surname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Incorrect data! Example: your1email@poshta.ua")]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [RegularExpression(@"\d{10}", ErrorMessage = "Incorrect data! Example:XXXXXXXXXX")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
