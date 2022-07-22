ac588dcc70b179706f2f27e605e67c9d4e8c2da1
IJchenko <zdiml72@gmail.com>
2022-07-22 11:18:54 +0300

    fix nullable type

--- CallBoardNix/Models/Advert/ResumeViewModel.cs
+++ CallBoardNix/Models/Advert/ResumeViewModel.cs
@@ -3,9 +3,9 @@
     public class ResumeViewModel
     {
         public Guid IdResume { get; set; }
-        public string Description { get; set; }
-        public string Login { get; set; }
+        public string? Description { get; set; }
+        public string? Login { get; set; }
         public Guid IdAdvert { get; set; }
-        public AdvertView Advert { get; set; }
+        public AdvertView? Advert { get; set; }
     }
 }
