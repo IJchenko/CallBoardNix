30bc2aeaccd216e1c07f161e59278485bfc380ba
IJchenko <zdiml72@gmail.com>
2022-07-22 11:09:27 +0300

    connecting services to a collection in a class

---
+++ CallBoardNix/Extentions/ServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Interfaces;
+using BusinessLayer.Services;
+using DataLayer.Repository;
+
+namespace CallBoardNix.Extentions
+{
+    public static class ServiceProvider
+    {
+        public static void AddDependency(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ICompanyService, CompanyService>();
+            services.AddScoped<IAdminService, AdminService>();
+            services.AddScoped<IRepository, GenericRepository>();
+        }
+    }
+}
